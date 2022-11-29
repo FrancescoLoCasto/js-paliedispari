cb2be8994e704ab48eda3bda4b1618dc21abee8b
MyUsername <my@email.com>
2022-11-29 16:29:37 +0100

    insert content on html file

---
+++ Odd or Even game/js/Script.js
@@ -0,0 +1,8 @@
+
+   /* Pari e Dispari
+   L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+   Generiamo un numero random (sempre da 1 a 5) per il computer attraverso una funzione.
+   Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari attraverso un'altra funzione
+   Dichiariamo chi ha vinto. */
+
+   
