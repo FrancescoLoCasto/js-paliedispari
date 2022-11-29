5149874f92d61c3fa1e45fb1a72402eae81ec74e
MyUsername <my@email.com>
2022-11-29 15:52:51 +0100

    Create prompt and fuction

---
+++ js/Script.js
@@ -0,0 +1,7 @@
+   /* Palidroma
+   Chiedere all’utente di inserire una parola:
+   Creare una funzione per capire se la parola inserita è palindroma */
+
+   /* Chiedere all'utente tramite un prompt una parola palindroma */
+
+   const userPalindromText = prompt ('Inserisci una parola palindroma!')
